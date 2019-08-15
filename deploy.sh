8c9229dc03f032b25a6a3126d526da31ca1297f5
sogrey <suzhaoheng2006@163.com>
2019-08-16 00:30:13 +0800

    deploy master

--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,16 @@
 # 确保脚本抛出遇到的错误
 set -e
 
+echo '【1】提交到 master'
+
 git init
 git add -A
 git commit -m 'deploy master'
 
 # 如果你想要部署到 https://USERNAME.github.io
 git push -f https://github.com/Sogrey/Three.js-start.git master
+
+echo '【2】生成静态文件'
 
 # 生成静态文件
 gitbook build
@@ -23,6 +27,8 @@ echo "commit.sh" >> .gitignore
 echo "deploy.sh" >> .gitignore
 
 git config --local http.postBuffer 524288000
+
+echo '【3】发布站点到 gh-pages 分支'
 
 git init
 git add -A
